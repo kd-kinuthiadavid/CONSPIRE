b5b1ac0c4da3a2bf28113a5e5ba5a24817bf3e4d
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2018-08-08 21:38:09 +0300

    edit urls.py - add urls for articles and feed

--- my_conspire/urls.py
+++ my_conspire/urls.py
@@ -5,8 +5,15 @@ from django.conf.urls.static import static
 from my_conspire import views
 
 urlpatterns=[
-        url(r'^$', views.welcome, name='welcome'),
-
+        url(r'^welcome', views.welcome, name='welcome'),
+        url(r'^$', views.all_feed, name='feed'),
+        url(r'^new/profile$', views.new_profile, name='new-profile'),
+        url(r'^all_profiles/(?P<profile_id>\d+)', views.all_profiles, name='all_profiles'),
+        url(r'^current_user_profile/(?P<profile_id>\d+)', views.current_user_profile, name='current_user_profile'),
+        url(r'^new/feed', views.new_feed, name='new-feed'),
+        url(r'^all_articles/', views.all_articles, name='all-articles'),
+        url(r'^new/article', views.new_article, name='new-article'),
+        url(r'^single_article/(?P<id>\d+)', views.single_article, name='single_article'),
 
 ]
 if settings.DEBUG:
