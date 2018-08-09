52986a579441bc5c8ecbf5a61c298ea6fa6138b1
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2018-08-09 19:36:57 +0300

    edit to add NewFeedComment

--- my_conspire/forms.py
+++ my_conspire/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from my_conspire.models import Profile, Feed, Article, Question, Answer
+from my_conspire.models import Profile, Feed, Article, Question, Answer, Comment, FeedComment
 
 
 class NewProfileForm(forms.ModelForm):
@@ -30,3 +30,14 @@ class NewAnswerForm(forms.ModelForm):
     class Meta:
         model = Answer
         exclude = ['comment']
+
+
+class NewCommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content', 'profile', 'article']
+
+class NewFeedCommentForm(forms.ModelForm):
+    class Meta:
+        model = FeedComment
+        fields = ['content', 'feed']
